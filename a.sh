d7eba405e1c8b787e1c06bf6c9e83f9162f3c5ce
lmaodark <lmao@gmail.com>
2025-03-09 22:32:24 +0700

    update

--- a.sh
+++ a.sh
@@ -1,7 +1,7 @@
-# apt install git python3 -y
+apt install meson -y
 git clone https://chromium.googlesource.com/chromium/tools/depot_tools -b main
 export PATH=$PATH:$(pwd)/depot_tools
-gclient sync
+fetch v8
 cd v8
 git checkout 4a03d61accede9dd0e3e6dc0456ff5a0e3f792b4
 gclient sync
