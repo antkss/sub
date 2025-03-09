a2f5ac945337b038f5b094154b8d253bb0aca6c6
lmaodark <lmao@gmail.com>
2025-03-09 22:11:33 +0700

    update

--- a.sh
+++ a.sh
@@ -1,6 +1,6 @@
 apt install git python3 -y
-git clone https://chromium.googlesource.com/chromium/tools/depot_tools /depot
-export PATH=$PATH:/depot
+git clone https://chromium.googlesource.com/chromium/tools/depot_tools -b main
+export PATH=$PATH:$(pwd)/depot_tools
 git checkout 4a03d61accede9dd0e3e6dc0456ff5a0e3f792b4
 git pull
 tools/dev/gm.py x64.release
