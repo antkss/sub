797dc6ab750fab8aa4164e97ad619f155d2ce189
lmaodark <lmao@gmail.com>
2025-03-09 22:15:05 +0700

    update

--- a.sh
+++ a.sh
@@ -3,4 +3,6 @@ git clone https://chromium.googlesource.com/chromium/tools/depot_tools -b main
 export PATH=$PATH:$(pwd)/depot_tools
 git checkout 4a03d61accede9dd0e3e6dc0456ff5a0e3f792b4
 git pull
-tools/dev/gm.py x64.release
+tools/dev/gm.py x64.release;
+pwd;
+ls
