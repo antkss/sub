882b8b668062a65e1f9e7ee5d4358c3735c3b68c
lmaodark <lmao@gmail.com>
2025-03-09 22:30:19 +0700

    update

--- a.sh
+++ a.sh
@@ -1,8 +1,25 @@
 # apt install git python3 -y
 git clone https://chromium.googlesource.com/chromium/tools/depot_tools -b main
 export PATH=$PATH:$(pwd)/depot_tools
+gclient sync
+cd v8
 git checkout 4a03d61accede9dd0e3e6dc0456ff5a0e3f792b4
-git pull
-tools/dev/gm.py x64.release;
-pwd;
-ls
+gclient sync
+gn gen x64.debug
+cd x64.debug
+echo "is_component_build = false
+is_debug = false
+target_cpu = "x64"
+v8_enable_sandbox = true
+
+v8_enable_backtrace = true
+v8_enable_disassembler = true
+v8_enable_object_print = true
+v8_enable_verify_heap = true
+
+v8_enable_memory_corruption_api = true
+" > args.gn
+
+ninja
+cd ..
+tar -cJf v8build.tar.xz x64.debug
